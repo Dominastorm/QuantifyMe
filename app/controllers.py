2dbf33bf442f34220b02811e0002050c5bfff32f
Dominastorm <dchawla228@gmail.com>
2022-03-18 19:14:46 +0530

    created basic pages

--- app/controllers.py
+++ app/controllers.py
@@ -6,3 +6,15 @@ from flask import current_app as app
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/home', methods=['GET'])
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/journal', methods=['GET'])
+def journal():
+    return render_template('journal.html')
+
+@app.route('/add-tracker', methods=['GET'])
+def add_tracker():
+    return render_template('add_tracker.html')
